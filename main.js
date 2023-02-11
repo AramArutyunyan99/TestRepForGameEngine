00cd42599c7640fa3afa3d404e2915cdd80c52f5
Casper <aru.aram99@gmail.com>
2023-02-12 00:10:16 +0400

    1st commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function CreateSprite(className, width, height, bg, sercle, pushTo)
 {
 	var _sprite = document.createElement('div');
-	_sprite.className.add(className);
+	_sprite.classList.add(className);
 	_sprite.style.width = width + 'px';
 	_sprite.style.height = height + 'px';
 	_sprite.style.background = bg;
