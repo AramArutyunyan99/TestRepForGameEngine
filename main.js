c6fa1a7ab3df2d72afe54c741388fee3e98c5fcb
Casper <aru.aram99@gmail.com>
2023-02-11 20:31:10 +0400

    a

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
-
-// //dadasdasdasdasd
-// class CreateSprite
-// {
-// 	constructor(className, width, height, bg, sercle, pushTo){
-// 		this.className = className;
-// 		this.width = width;
-// 		this.height = height;
-// 		this.bg = bg;
-// 		this.sercle = sercle;
-// 		this.pushTo = pushTo;
-// 	}
-// 	createColorBlock() {
-// 		var _block = document.createElement('div');
-// 		_block.classList.add(this.className);
-// 		_block.style.width = this.width + 'px';
-// 		_block.style.height = this.height + 'px';
-// 		_block.style.background = this.bg;
-// 		if (this.sercle == true){
-// 			_block.style.borderRadius = 100 + '%';
-// 		}
-// 		this.pushTo.append(_block);
-// 	}
-// }
-
-function sh(text) {
-	console.log(text);
+export class CreateSprite
+{
+	constructor(className, width, height, bg, sercle, pushTo){
+		this.className = className;
+		this.width = width;
+		this.height = height;
+		this.bg = bg;
+		this.sercle = sercle;
+		this.pushTo = pushTo;
+	}
+	createColorBlock() {
+		var _block = document.createElement('div');
+		_block.classList.add(this.className);
+		_block.style.width = this.width + 'px';
+		_block.style.height = this.height + 'px';
+		_block.style.background = this.bg;
+		if (this.sercle == true){
+			_block.style.borderRadius = 100 + '%';
+		}
+		this.pushTo.append(_block);
+	}
 }
+
